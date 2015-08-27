d6934ce6744e63436d07151ca12d73b424ab3be2
bondparkerbond <parkerbond@gmail.com>
2015-08-27 11:58:02 -0600

    First Web App

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body)
+  end#this method is like a new attr_accessor?
+
+end #end of PostsController Class
